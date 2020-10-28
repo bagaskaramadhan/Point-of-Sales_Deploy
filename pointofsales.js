a5797e221f7585ab27ed1ef7afa55bfaa03d1750
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-10-28 16:42:46 +0700

    aws

--- pointofsales.js
+++ pointofsales.js
@@ -11,6 +11,6 @@ app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8002
 app.listen(port)
 console.log(`app is listening on port: ${port}`)

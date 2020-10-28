7a07830c4e7eaf325b33c547ff6ece923227ac47
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-10-28 16:53:51 +0700

    aws

--- pointofsales.js
+++ pointofsales.js
@@ -1,10 +1,11 @@
 const express = require('express')
+// const express.static = require('serve-static')
 const path = require('path')
 
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', express.static(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
